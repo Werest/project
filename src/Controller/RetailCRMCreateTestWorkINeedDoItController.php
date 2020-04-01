f6e764eff37ef20c16dd66f6bcaba29297f5a60a
werest <weresttrade@ya.ru>
2020-04-01 16:50:45 +0300

    add DB

--- src/Controller/RetailCRMCreateTestWorkINeedDoItController.php
+++ src/Controller/RetailCRMCreateTestWorkINeedDoItController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Ola;
 use RetailCrm\Exception\CurlException;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use RetailCrm\ApiClient;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class RetailCRMCreateTestWorkINeedDoItController
+class RetailCRMCreateTestWorkINeedDoItController extends AbstractController
 {
     public $url = 'https://weresttalk.retailcrm.ru/';
     public $key = 'iuGaclQUT9vJruuDPFv56CZMVdd43nRE';
@@ -37,7 +38,25 @@ class RetailCRMCreateTestWorkINeedDoItController
             $active = $this->client->request->integrationModulesGet($code)->getResponse();
 
             //2 non DB
-            $active = $active['integrationModule']['actions']['activity'];
+            $act = $active['integrationModule']['actions']['activity'];
+
+            // 2 - 3 - with DB
+            $code = $active['integrationModule']['code'];
+            $name = $active['integrationModule']['name'];
+            $logo = $active['integrationModule']['logo'];
+            $freeze = $active['integrationModule']['freeze'];
+
+            $entM = $this->getDoctrine()->getManager();
+
+            $ola = new Ola();
+            $ola->setCode($code);
+            $ola->setName($name);
+            $ola->setLogo($logo);
+            $ola->setActive($act);
+            $ola->setFreeze($freeze);
+
+            $entM->persist($ola);
+            $entM->flush();
 
 
 
@@ -45,10 +64,9 @@ class RetailCRMCreateTestWorkINeedDoItController
             echo "Connection error: " . $e->getMessage();
         }
 
-
-
         return $this->render('retail_crm_create_test_work_i_need_do_it/index.html.twig', [
-            'controller_name' => 'RetailCRMCreateTestWorkINeedDoItController'
+            'controller_name' => 'RetailCRMCreateTestWorkINeedDoItController',
+            'ola' => $ola->getId()
         ]);
     }
 
