f88f069abd7764806817cf17b606e3141c08c619
werest <weresttrade@ya.ru>
2020-04-01 20:01:49 +0300

    fix - Controller - 1

--- src/Controller/RetailCRMCreateTestWorkINeedDoItController.php
+++ src/Controller/RetailCRMCreateTestWorkINeedDoItController.php
@@ -3,65 +3,69 @@
 namespace App\Controller;
 
 use App\Entity\Ola;
+use App\Entity\TinyKangaroo;
 use RetailCrm\Exception\CurlException;
 use RetailCrm\ApiClient;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RetailCRMCreateTestWorkINeedDoItController extends AbstractController
 {
-    public $url = 'https://weresttalk.retailcrm.ru/';
-    public $key = 'iuGaclQUT9vJruuDPFv56CZMVdd43nRE';
     /**
      * @var ApiClient
      */
-    public $client;
-
+    private $client;
 
     public function index()
     {
-        $this->client = new ApiClient($this->url, $this->key);
+        //url and key select from db
 
-        try {
-            $code = 'consultant';
-            //1 - non DB
-            $r = array(
-                "code" => "awesometransport-101",
-                "name" => "Awesome Transport",
-                "logo"=> "https://image.flaticon.com/icons/svg/1793/1793188.svg",
-                "clientId" => 10,
-                "active" => true
-
-            );
-            $this->client->request->integrationModulesEdit($r);
-
-            $active = $this->client->request->integrationModulesGet($code)->getResponse();
-
-            //2 non DB
-            $act = $active['integrationModule']['actions']['activity'];
-
-            // 2 - 3 - with DB
-            $code = $active['integrationModule']['code'];
-            $name = $active['integrationModule']['name'];
-            $logo = $active['integrationModule']['logo'];
-            $freeze = $active['integrationModule']['freeze'];
-
-            $entM = $this->getDoctrine()->getManager();
-
-            $ola = new Ola();
-            $ola->setCode($code);
-            $ola->setName($name);
-            $ola->setLogo($logo);
-            $ola->setActive($act);
-            $ola->setFreeze($freeze);
-
-            $entM->persist($ola);
-            $entM->flush();
+        $rep = $this->getDoctrine()->getRepository(TinyKangaroo::class)->findAll();
+        foreach ($rep as $item) {
+            $url = $item->getUrl();
+            $key = $item->getK();
 
 
+            $this->client = new ApiClient($url, $key);
 
-        } catch (CurlException $e) {
-            echo "Connection error: " . $e->getMessage();
+            try {
+                $code = 'consultant';
+                //1 - non DB
+                $r = array(
+                    "code" => "awesometransport-101",
+                    "name" => "Awesome Transport",
+                    "logo" => "https://image.flaticon.com/icons/svg/1793/1793188.svg",
+                    "clientId" => 10,
+                    "active" => true
+
+                );
+                $this->client->request->integrationModulesEdit($r);
+
+                $active = $this->client->request->integrationModulesGet($code)->getResponse();
+
+
+                // 2 - 3 - with DB
+                $code = $active['integrationModule']['code'];
+                $name = $active['integrationModule']['name'];
+                $logo = $active['integrationModule']['logo'];
+                $freeze = $active['integrationModule']['freeze'];
+                $act = $active['integrationModule']['actions']['activity'];
+
+                $entM = $this->getDoctrine()->getManager();
+
+                $ola = new Ola();
+                $ola->setCode($code);
+                $ola->setName($name);
+                $ola->setLogo($logo);
+                $ola->setActive($act);
+                $ola->setFreeze($freeze);
+
+                $entM->persist($ola);
+                $entM->flush();
+
+
+            } catch (CurlException $e) {
+                echo "Connection error: " . $e->getMessage();
+            }
         }
 
         return $this->render('retail_crm_create_test_work_i_need_do_it/index.html.twig', [
@@ -69,6 +73,4 @@ class RetailCRMCreateTestWorkINeedDoItController extends AbstractController
             'ola' => $ola->getId()
         ]);
     }
-
-
 }
